2d664cec0649191cbf1b128a060361584827af6e
fuk665 <643707579@qq.com>
2016-08-01 22:08:57 +0800

    Update MyServerSocket.java

--- Exam2/src/main/java/com/hand/MyServerSocket.java
+++ Exam2/src/main/java/com/hand/MyServerSocket.java
@@ -5,7 +5,7 @@ public class MyServerSocket {
 	public static void main(String[] args) {
 		
 		new ServerListener().start();
-		System.out.println("OK,请输入nettel localhost:12345或者地址栏输入localhost:12345");
+		System.out.println("The client is connected, please enter the localhost:12345 in the address bar.");
 		
 	}
 	

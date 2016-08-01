4c6ecdb2cabfc99bde7e5d09588d1196d5e7444d
fuk665 <643707579@qq.com>
2016-08-01 21:49:29 +0800

    Update MyServerSocket.java

--- Exam2/src/main/java/com/hand/MyServerSocket.java
+++ Exam2/src/main/java/com/hand/MyServerSocket.java
@@ -5,6 +5,7 @@ public class MyServerSocket {
 	public static void main(String[] args) {
 		
 		new ServerListener().start();
+		System.out.println("OK,请输入nettel localhost:12345或者地址栏输入localhost:12345");
 		
 	}
 	
